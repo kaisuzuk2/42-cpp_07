dd89b41ade0d68abad31fdbbf3713ebdf3bb7c3b
Kaisei Suzuki <kaisuzuk@c4r3s17.42tokyo.jp>
2026-02-28 09:34:19 +0900

    stringを特殊化

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 08:20:27 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2026/02/24 08:35:01 by kaisuzuk         ###   ########.fr       */
+/*   Updated: 2026/02/28 09:32:02 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ void printConst(const T &x) {
 template <typename T>
 void increment(T &x) {
 	++x;
+}
+template <>
+void increment<std::string>(std::string &s) {
+	std::cout << s << " modified" << std::endl;
 }
 
 void header(int id, const std::string &title) {
@@ -64,7 +68,32 @@ int main(void) {
 	}
 
 	{
-		header(2, "const int");
+		header(2, "char");
+		sec("print");
+		char arr[] = {'a', 'b', 'c', 'd'};
+		::iter(arr, sizeof(arr) / sizeof(arr[0]), print<char>);
+		std::cout << std::endl;
+
+		sec("increment");
+		::iter(arr, sizeof(arr) / sizeof(arr[0]), increment<char>);
+		::iter(arr, sizeof(arr) / sizeof(arr[0]), print<char>);
+	}
+
+	{
+		header(3, "string");
+		sec("print");
+		std::string arr[] = {"hello", "42", "tokyo"};
+		::iter(arr, sizeof(arr) / sizeof(arr[0]), print<std::string>);
+		std::cout << std::endl;
+
+		sec("increment");
+		::iter(arr, sizeof(arr) / sizeof(arr[0]), increment<std::string>);
+		::iter(arr, sizeof(arr) / sizeof(arr[0]), print<std::string>);
+		std::cout << std::endl;
+	}
+
+	{
+		header(4, "const int");
 		sec("print");
 		const int arr[] = {1, 2, 3, 4, 5};
 		::iter(arr, sizeof(arr) / sizeof(arr[0]), printConst<int>);
